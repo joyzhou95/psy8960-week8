6935cbe6e304fbdffee3c4011a9e16bb0f9ee771
Joy Zhou <zhou1559@umn.edu>
2023-03-19 01:32:11 -0500

    Simplied codes

--- shiny/app.R
+++ shiny/app.R
@@ -1,7 +1,7 @@
 library(shiny)
 # Load tidyverse package for data importing
 library(tidyverse)
-week8_tbl <- readRDS("../shiny/week8_tbl.Rds")
+
 ui <- fluidPage(
   selectInput("gender", "Select gender if you only want to see one gender's data",
               selected = "All", choices = c("Male", "Female", "All")),
@@ -13,74 +13,40 @@ ui <- fluidPage(
   # Use plotOutput to create a non-interactive plot output 
   plotOutput("fig")
 )
-
 server <- function(input, output) {
   output$fig <- renderPlot({
-    if (input$completetime == "Include"){
-      if (input$errorband == "Display Error Band"){
-        if (input$gender == "All") {
+    week8_tbl <- read_rds("./week8.rds")
+    if (input$completetime == "Exclude"){
+      week8_tbl %>% filter(timeEnd > "2017-08-01")} else
+      {week8_tbl %>%
+          ggplot(aes(q1_q6_mean, q8_q10_mean)) +
+          geom_point(position = "jitter") +
+          geom_smooth(method = "lm", color = "purple")
+      } 
+    if (input$errorband == "Suppress Error Band"){
+      week8_tbl %>%
+        ggplot(aes(q1_q6_mean, q8_q10_mean)) +
+        geom_point(position = "jitter") +
+        geom_smooth(method = "lm", color = "purple", se = F)} else {
           week8_tbl %>%
             ggplot(aes(q1_q6_mean, q8_q10_mean)) +
             geom_point(position = "jitter") +
             geom_smooth(method = "lm", color = "purple")
-        } else {
+        }
+    if (input$gender != "All") {
           week8_tbl %>%
             filter(gender == input$gender) %>%
             ggplot(aes(q1_q6_mean, q8_q10_mean)) +
             geom_point(position = "jitter") +
             geom_smooth(method = "lm", color = "purple") 
-        }} else {
-          if (input$gender == "All") {
+        } else {
             week8_tbl %>%
               ggplot(aes(q1_q6_mean, q8_q10_mean)) +
               geom_point(position = "jitter") +
-              geom_smooth(method = "lm", color = "purple", se = F)
-          } else {
-            week8_tbl %>%
-              filter(gender == input$gender) %>%
-              ggplot(aes(q1_q6_mean, q8_q10_mean)) +
-              geom_point(position = "jitter") +
-              geom_smooth(method = "lm", color = "purple", se = F)
-          }}} else {
-            if (input$errorband == "Display Error Band"){
-              if (input$gender == "All") {
-                week8_tbl %>%
-                  filter(timeEnd > "2017-08-01") %>%
-                  ggplot(aes(q1_q6_mean, q8_q10_mean)) +
-                  geom_point(position = "jitter") +
-                  geom_smooth(method = "lm", color = "purple")
-              } else {
-                week8_tbl %>%
-                  filter(timeEnd > "2017-08-01") %>%
-                  filter(gender == input$gender) %>%
-                  ggplot(aes(q1_q6_mean, q8_q10_mean)) +
-                  geom_point(position = "jitter") +
-                  geom_smooth(method = "lm", color = "purple") 
-              }} else {
-                if (input$gender == "All") {
-                  week8_tbl %>%
-                    filter(timeEnd > "2017-08-01") %>%
-                    ggplot(aes(q1_q6_mean, q8_q10_mean)) +
-                    geom_point(position = "jitter") +
-                    geom_smooth(method = "lm", color = "purple", se = F)
-                } else {
-                  week8_tbl %>%
-                    filter(timeEnd > "2017-08-01") %>%
-                    filter(gender == input$gender) %>%
-                    ggplot(aes(q1_q6_mean, q8_q10_mean)) +
-                    geom_point(position = "jitter") +
-                    geom_smooth(method = "lm", color = "purple", se = F)
-                }}}
+              geom_smooth(method = "lm", color = "purple")
+          }          
   })
 }
-
 shinyApp(ui = ui, server = server)
-
-library(rsconnect)
-rsconnect::setAccountInfo(name='joyzhou', 
-                          token='2590A8DB5F2C12C2946B93E305228F9D', 
-                          secret='Fr2jmw3fmOMtVIOpKGiVsENgS+9a0TTG/dOe+ll9')
-
-rsconnect::deployApp(appName = "shiny_week8", appDir = "../shiny")
 
 
