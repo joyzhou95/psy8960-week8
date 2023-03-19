702afa5540c434d91c40bd3f6a26094988a0f6c8
Joy Zhou <zhou1559@umn.edu>
2023-03-18 20:02:36 -0500

    Created input and output for participants inclusion

--- shiny/app.R
+++ shiny/app.R
@@ -11,38 +11,70 @@ ui <- fluidPage(
   selectInput("errorband", "Select display or suppress error band", 
               selected = "Display Error Band", choices = c("Display Error Band",
                                                            "Suppress Error Band")),
+  selectInput("completetime", "Select include or exclude participants who completed assessments before Aug. 1, 2017",
+              selected = "Include", choices = c("Include", "Exclude")),
   # Use plotOutput to create a non-interactive plot output 
   plotOutput("fig")
 )
 
 server <- function(input, output) {
   output$fig <- renderPlot({
-    if (input$errorband == "Display Error Band"){
-      if (input$gender == "All") {
-        week8_tbl %>%
-          ggplot(aes(q1_q6_mean, q8_q10_mean)) +
-          geom_point(position = "jitter") +
-          geom_smooth(method = "lm", color = "purple")
-      } else {
-        week8_tbl %>%
-          filter(gender == input$gender) %>%
-          ggplot(aes(q1_q6_mean, q8_q10_mean)) +
-          geom_point(position = "jitter") +
-          geom_smooth(method = "lm", color = "purple") 
-      }} else {
+    if (input$completetime == "Include"){
+      if (input$errorband == "Display Error Band"){
         if (input$gender == "All") {
           week8_tbl %>%
             ggplot(aes(q1_q6_mean, q8_q10_mean)) +
             geom_point(position = "jitter") +
-            geom_smooth(method = "lm", color = "purple", se = F)
+            geom_smooth(method = "lm", color = "purple")
         } else {
           week8_tbl %>%
             filter(gender == input$gender) %>%
             ggplot(aes(q1_q6_mean, q8_q10_mean)) +
             geom_point(position = "jitter") +
-            geom_smooth(method = "lm", color = "purple", se = F)
-        }}
-    })
+            geom_smooth(method = "lm", color = "purple") 
+        }} else {
+          if (input$gender == "All") {
+            week8_tbl %>%
+              ggplot(aes(q1_q6_mean, q8_q10_mean)) +
+              geom_point(position = "jitter") +
+              geom_smooth(method = "lm", color = "purple", se = F)
+          } else {
+            week8_tbl %>%
+              filter(gender == input$gender) %>%
+              ggplot(aes(q1_q6_mean, q8_q10_mean)) +
+              geom_point(position = "jitter") +
+              geom_smooth(method = "lm", color = "purple", se = F)
+          }}} else {
+            if (input$errorband == "Display Error Band"){
+              if (input$gender == "All") {
+                week8_tbl %>%
+                  filter(timeEnd > "2017-08-01") %>%
+                  ggplot(aes(q1_q6_mean, q8_q10_mean)) +
+                  geom_point(position = "jitter") +
+                  geom_smooth(method = "lm", color = "purple")
+              } else {
+                week8_tbl %>%
+                  filter(timeEnd > "2017-08-01") %>%
+                  filter(gender == input$gender) %>%
+                  ggplot(aes(q1_q6_mean, q8_q10_mean)) +
+                  geom_point(position = "jitter") +
+                  geom_smooth(method = "lm", color = "purple") 
+              }} else {
+                if (input$gender == "All") {
+                  week8_tbl %>%
+                    filter(timeEnd > "2017-08-01") %>%
+                    ggplot(aes(q1_q6_mean, q8_q10_mean)) +
+                    geom_point(position = "jitter") +
+                    geom_smooth(method = "lm", color = "purple", se = F)
+                } else {
+                  week8_tbl %>%
+                    filter(timeEnd > "2017-08-01") %>%
+                    filter(gender == input$gender) %>%
+                    ggplot(aes(q1_q6_mean, q8_q10_mean)) +
+                    geom_point(position = "jitter") +
+                    geom_smooth(method = "lm", color = "purple", se = F)
+                }}}
+  })
 }
 
 shinyApp(ui = ui, server = server)
