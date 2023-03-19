1f0eec08eab500ecab011450478b08dca9a47647
Joy Zhou <zhou1559@umn.edu>
2023-03-18 19:57:40 -0500

    Created input and output for gender selection

--- shiny/app.R
+++ shiny/app.R
@@ -6,15 +6,27 @@ library(tidyverse)
 week8_tbl <- read_csv(file = "../data/week8_tbl.csv")
 
 ui <- fluidPage(
-
+  selectInput("gender", "Select gender if you only want to see one gender's data",
+              selected = "All", choices = c("Male", "Female", "All")),
   # Use plotOutput to create a non-interactive plot output 
   plotOutput("fig")
 )
 
 server <- function(input, output) {
   output$fig <- renderPlot({
-   
-  })
+    if (input$gender == "All") {
+      week8_tbl %>%
+        ggplot(aes(q1_q6_mean, q8_q10_mean)) +
+        geom_point(position = "jitter") +
+        geom_smooth(method = "lm", color = "purple")
+    } else {
+      week8_tbl %>%
+        filter(gender == input$gender) %>%
+        ggplot(aes(q1_q6_mean, q8_q10_mean)) +
+        geom_point(position = "jitter") +
+        geom_smooth(method = "lm", color = "purple") 
+  }
+ })
 }
 
 shinyApp(ui = ui, server = server)
